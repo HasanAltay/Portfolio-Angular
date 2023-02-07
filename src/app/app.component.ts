e8d23d3c927262fcb0825519afe1921098048f94
HasanAltay <altay@posteo.de>
2023-02-07 19:38:39 +0100

    animations import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my Portfolio';
+
+
+  ngOnInit(): void {
+    AOS.init()
+  }
 
   setActive(num: number) {
     const aboutBtn = document.getElementById('about_btn');
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.scss']
 })
-export class MainpageComponent {
+export class MainpageComponent{
 
 }
